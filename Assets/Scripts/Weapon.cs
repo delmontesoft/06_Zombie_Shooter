5e0e0befb5d0b865084a8d5155f21b2750438840
Rodrigo Montes <rjmontes@gmail.com>
2020-04-14 13:30:36 -0400

    Video 177 - Give the man a gun!
    - Added carabine to player
    - Added gun reticle to UI
    - Added raycast to detect hits of the gun
    - Enemy health decresed when shot and destroyed when health reaches 0

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPcamera;
+    [SerializeField] float weaponRange = 100f;
+    [SerializeField] float weaponDamage = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(FPcamera.transform.position, FPcamera.transform.forward, out hit, weaponRange))
+        {
+            //TODO add some hit effect
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (!target) return;
+            target.TakeDamage(weaponDamage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
