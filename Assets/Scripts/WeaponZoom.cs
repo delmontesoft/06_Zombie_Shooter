6d02cbd523ae01992ee0a1b5d271396fe699dea1
Rodrigo Montes <rjmontes@gmail.com>
2020-04-23 12:56:53 -0400

    Video 193 - Sensible weapon
    - Adjusted weapon (mouse look) sensitivity when zoomed in (values exposed as a serialized field)

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
+    //TODO define weapon zoom sensitivity with a formula or a value that is weapon dependant
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 1.5f;
+
     bool isZoomed = false;
+    RigidbodyFirstPersonController fpsController = null;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     public void ToggleZoom (float weaponZoomRange)
     {
         if (!isZoomed)
         {
             Camera.main.fieldOfView -= weaponZoomRange;
+            fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
         }
         else
         {
             Camera.main.fieldOfView += weaponZoomRange;
+            fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
         }
         isZoomed = !isZoomed;
     }
