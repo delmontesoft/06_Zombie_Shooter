5485089f600f8a23c62e30dee6968c952440e4dd
Rodrigo Montes <rjmontes@gmail.com>
2020-05-26 12:37:48 -0400

    Video 213 - Flashlight
    - Added flashligth with batteries running out over time

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float intensityDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 30f;
+
+    Light myFlashlight;
+
+    private void Start()
+    {
+        myFlashlight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myFlashlight.spotAngle > minimumAngle)
+        {
+            myFlashlight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myFlashlight.intensity -= intensityDecay * Time.deltaTime;
+    }
+}
